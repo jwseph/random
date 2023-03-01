2cae5ec14de0cd5c5f536fbee711686bc39032f9
jwseph <kodomo.oishii@gmail.com>
2023-02-28 21:04:52 -0800

    Speed improvement using sorting

--- math_bowling.py
+++ math_bowling.py
@@ -1,6 +1,3 @@
-# Todo speed optimization: sort all the exps in order from shortest to longest, then break after all the numbers have been solved
-# - Around the same speed for difficult cases but much faster for easy cases
-
 from math import isclose, factorial, sqrt as s
 import re
 import time
@@ -10,23 +7,28 @@ f = lambda x: factorial(int(x)) if x.is_integer() and 0 <= x <= 6 else float('na
 comm_ops = '+', '*'
 ops = '-', '/', '**', '%'
 
-funcs1 = [('', ''), ('s(', ')')], [('', ''), ('s(', ')'), ('f(', ')')]
+funcs1 = {
+    False: [('(', ')'), ('s(', ')')],
+    True: [('(', ')'), ('s(', ')'), ('f(', ')')],
+}
+funcs2 = {
+    False: [('', ''), ('(', ')'), ('s(', ')')],
+    True: [('', ''), ('(', ')'), ('s(', ')'), ('f(', ')')],
+}
 
-funcs2 = [('', ''), ('(', ')'), ('s(', ')')], [('', ''), ('(', ')'), ('s(', ')'), ('f(', ')')]
-
-def get_exps(nums: set[int], mode: int):
+def get_exps(nums: set[int], use_fact: bool) -> dict:
     assert len(nums) > 0
     if len(nums) == 1:
         num = next(iter(nums))
         return dict.fromkeys(
             f'{f_l}{num}{f_r}'
-            for f_l, f_r in funcs1[mode]
+            for f_l, f_r in funcs1[use_fact]
         )
     
     res = {}
     for num in nums:
-        a_res = get_exps({num}, mode)
-        b_res = get_exps(nums-{num}, mode)
+        a_res = get_exps({num}, use_fact)
+        b_res = get_exps(nums-{num}, use_fact)
         for op in comm_ops:
             for a in a_res:
                 for b in b_res:
@@ -39,10 +41,13 @@ def get_exps(nums: set[int], mode: int):
     return dict.fromkeys(
         f'{f_l}{_}{f_r}'
         for _ in res
-        for f_l, f_r in (funcs2 if len(nums) < 3 else funcs1)[mode]
+        for f_l, f_r in (funcs2 if len(nums) < 3 else funcs1)[use_fact]
     )
 
-def reverse_fact(exp):
+def reverse_fact(exp: str) -> str:
+    '''
+    Converts a string in the format `f(3+5)*f2` to `(3+5)!*2!` (slow!)
+    '''
     res = ''
     s = []
     for ch in exp:
@@ -57,28 +62,40 @@ def reverse_fact(exp):
             s.pop()
     return res
 
-def solve(nums: set[int], *, mode: int):
+def solve(nums: set[int], *, use_fact: bool = True):
+    '''
+    Prints shortest solutions to `nums` for the integers [1, 10]
+    :param use_fact: (False) sqrt only or (True) sqrt and factorial
+    '''
     t = time.perf_counter()
+    exps = list(get_exps(nums, use_fact))
+    print('Sorting', len(exps), 'expressions...')
+    exps.sort(key=lambda exp: len(exp)-exp.count('**')-2*int(exp.startswith('(')))
+    # Sorting drastically improves speed for most cases,
+    # while worsening speed for unsolvable cases
 
-    exps = get_exps(nums, mode)
     print('Evaluating', len(exps), 'expressions...')
-
     res = {(i+1): None for i in range(10)}
+    left = len(res)
     for exp in exps:
         try:
             ev = eval(exp)
             num = int(ev+.5)
-            exp = exp.replace('**', '^').replace('.0', '')
-            exp = re.sub(r'(s|f)\((\d)\)', r'\1\2', exp)
-            if mode == 1: exp = reverse_fact(exp)
             if not isclose(ev, num) or not num in res: continue
-            if res[num] is None or len(exp) < len(res[num]):
+            if res[num] is None:
                 res[num] = exp
+                left -= 1
+                if left == 0: break
         except:
             pass
     for num, exp in res.items():
-        if res[num] is None: exp = ''
-        res[num] = exp.replace('s', '√')
+        if exp is None: exp = ' '
+        exp = exp.replace('**', '^').replace('s', '√')
+        exp = re.sub(r'\((\d)\.0\)', r'\1', exp)
+        if exp[0] == '(' and exp[-1] ==')':
+            exp = exp[1:-1]
+        if use_fact: exp = reverse_fact(exp)
+        res[num] = exp
 
     w = max(len(f'{num} = {exp}') for num, exp in res.items())
     print('+'+'-'*(2+w)+'+')
@@ -87,14 +104,10 @@ def solve(nums: set[int], *, mode: int):
         for num, exp in res.items()
     ))
     print('+'+'-'*(2+w)+'+')
-    print(f'Time taken: {time.perf_counter()-t:.3f}s')
+    print(f'Completed in {time.perf_counter()-t:.3f}s')
+
 
 if __name__ == '__main__':
-    # nums = {float(_) for _ in input('Enter nums (space-separated): ').split()}
-    # solve(nums, mode=1)
-    # exit()
-    for a in range(1, 10):
-        for b in range(a+1, 10):
-            for c in range(b+1, 10):
-                print([a, b, c])
-                solve({a, b, c}, mode=1)
+    nums = {float(_) for _ in input('Enter nums (space-separated): ').split()}
+    solve(nums, use_fact=True)
+    exit()
