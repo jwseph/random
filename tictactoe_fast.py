90e4f062d92d0af788aff98fb6d074097850527f
jwseph <kodomo.oishii@gmail.com>
2023-03-26 21:50:22 -0700

    Fixed pruning, 0.0025

--- tictactoe_fast.py
+++ tictactoe_fast.py
@@ -15,12 +15,13 @@ win = [
 dp = {}
 def sol(a=0, b=0, m=-1, M=1, l=9):
     if (a, b) in dp: return dp[a, b]
-    if m >= M: return M
+    if m >= M: return m
     for s in win[l]:
         if a&s == s: return 1
     if a|b == (1<<9)-1: return 0
     for i in range(9):
         if a&1<<i|b&1<<i: continue
         m = max(m, -sol(b, a|1<<i, -M, -m, i))
+        if m >= M: break
     dp[a, b] = m
     return m
